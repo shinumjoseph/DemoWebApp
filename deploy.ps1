c968b78f164e2faba9ee8c0cd7edb43fb8c10715
shinumjoseph <63649276+shinumjoseph@users.noreply.github.com>
2026-06-29 23:13:00 +0530

    Add files via upload

---
+++ deploy.ps1
@@ -0,0 +1,22 @@
+Import-Module WebAdministration
+
+$publishFolder = "C:\Users\hp\Desktop\DevOps\DemoWebApp\DemoWebAppPublish"
+$destination = "C:\DevOps Projects\DemoWebApps"
+
+Write-Host "Stopping IIS Website..."
+Stop-WebSite -Name "DemoWebApps"
+
+Write-Host "Cleaning deployment folder..."
+if (Test-Path $destination) {
+    Remove-Item "$destination\*" -Recurse -Force
+} else {
+    New-Item -ItemType Directory -Path $destination
+}
+
+Write-Host "Copying published files..."
+Copy-Item "$publishFolder\*" $destination -Recurse -Force
+
+Write-Host "Starting IIS Website..."
+Start-WebSite -Name "DemoWebApps"
+
+Write-Host "Deployment completed."
